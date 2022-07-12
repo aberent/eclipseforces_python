6300ccb3592fa54b2b0ece6ce992fc6baa69d5c9
Anthony Berent <berentanthony@gmail.com>
2022-07-12 14:47:26 +0100

    Save prefs link to problem statement between Eclipse runs
    Flush the prefs so that they are saved.
    
    Also some minor code tidying

--- eclipseforces_python/src/io/github/aberent/eclipseforces/python/PythonCodeGenerator.java
+++ eclipseforces_python/src/io/github/aberent/eclipseforces/python/PythonCodeGenerator.java
@@ -1,3 +1,19 @@
+/*   
+   Copyright 2022 Anthony Berent
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package io.github.aberent.eclipseforces.python;
 
 import java.util.List;
@@ -5,38 +21,31 @@ import java.util.List;
 import net.fmoraes.eclipseforces.ProblemStatement;
 import net.fmoraes.eclipseforces.languages.CodeGenerator;
 
+/**
+ * @author anthony
+ * 
+ *         Python code generator for Eclipse Forces
+ *
+ */
 public class PythonCodeGenerator extends CodeGenerator {
-	
-	public static final String DEFAULT_CODE_TEMPLATE = "def main():\n"
-			+ "    pass\n"
-			+ "\n"
-			+ "\n"
-			+ "if __name__ == \"__main__\":\n"
-			+ "    main()\n";
-	
+
+	public static final String DEFAULT_CODE_TEMPLATE = "def main():\n    pass\n\n\nif __name__ == \"__main__\":\n    main()\n";
+
 	public PythonCodeGenerator(List<ProblemStatement> problems) {
 		super(problems);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public String getTestsSource(ProblemStatement ps) {
 		StringBuilder testFileBuilder = new StringBuilder();
 		testFileBuilder.append(
-				"import sys\n"
-				+ "from unittest import TestCase\n"
-				+ "from io import StringIO\n"
-				+ "from contextlib import redirect_stdout\n"
-				+ "from ");
+				"import sys\nfrom unittest import TestCase\nfrom io import StringIO\nfrom contextlib import redirect_stdout\nfrom ");
 		String className = ps.getClassName();
 		testFileBuilder.append(className);
-		testFileBuilder.append(" import main\n"
-				+ "\n"
-				+ "\n"
-				+ "class Test(TestCase):\n");
-	    int i = -1;
-	    int timeLimit = Integer.parseInt(ps.getTimeLimit());
-		for (ProblemStatement.TestCase testCase: ps.getTestCases() ) {
+		testFileBuilder.append(" import main\n\n\nclass Test(TestCase):\n");
+		int i = -1;
+		int timeLimit = Integer.parseInt(ps.getTimeLimit());
+		for (ProblemStatement.TestCase testCase : ps.getTestCases()) {
 			i++;
 			getTestCaseSource(testFileBuilder, className, timeLimit, i, testCase.getInput(), testCase.getOutput());
 		}
@@ -46,24 +55,24 @@ public class PythonCodeGenerator extends CodeGenerator {
 	@Override
 	public void getTestCaseSource(StringBuilder result, String className, int timeLimit, int number, String input,
 			String output) {
-				if (input.charAt(0) == '\n') {
-					input = input.substring(1);
-				}
-				if (output.charAt(0) == '\n') {
-					output= output.substring(1);
-				}
-				result.append("    def test_");
-				result.append(number);
-				result.append("(self):\n");
-				result.append("        sys.stdin = StringIO('''");
-				result.append(input);
-				result.append("'''\n)\n");
-				result.append("        with redirect_stdout(StringIO()) as out:\n");
-				result.append("            main()\n");
-				result.append("        sys.stdin = sys.__stdin__\n");
-				result.append("        self.assertEqual('''");
-				result.append(output);
-				result.append("''',out.getvalue())\n\n");
+		if (input.charAt(0) == '\n') {
+			input = input.substring(1);
+		}
+		if (output.charAt(0) == '\n') {
+			output = output.substring(1);
+		}
+		result.append("    def test_");
+		result.append(number);
+		result.append("(self):\n");
+		result.append("        sys.stdin = StringIO('''");
+		result.append(input);
+		result.append("'''\n)\n");
+		result.append("        with redirect_stdout(StringIO()) as out:\n");
+		result.append("            main()\n");
+		result.append("        sys.stdin = sys.__stdin__\n");
+		result.append("        self.assertEqual('''");
+		result.append(output);
+		result.append("''',out.getvalue())\n\n");
 	}
 
 }
--- eclipseforces_python/src/io/github/aberent/eclipseforces/python/PythonLanguageSupport.java
+++ eclipseforces_python/src/io/github/aberent/eclipseforces/python/PythonLanguageSupport.java
@@ -1,3 +1,19 @@
+/*   
+   Copyright 2022 Anthony Berent
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package io.github.aberent.eclipseforces.python;
 
 import java.io.ByteArrayInputStream;
@@ -15,7 +31,7 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 
 public class PythonLanguageSupport extends LanguageSupport {
-	
+
 	@Override
 	protected CodeGenerator createCodeGenerator(List<ProblemStatement> tasks) {
 		return new PythonCodeGenerator(tasks);
@@ -24,17 +40,20 @@ public class PythonLanguageSupport extends LanguageSupport {
 	@Override
 	protected IFile createLanguageProject(IProject project, ProblemStatement problem, Contest contest)
 			throws Exception {
-	    IEclipsePreferences prefs = EclipseForcesPlugin.getProjectPrefs(project);
-	    prefs.put(ProblemStatementView.CONTEST_KEY, contest.name);
-	    List<IFile> testFiles = new ArrayList<IFile>();
-		for (ProblemStatement ps: getProblems()) {
-			prefs.put(ps.getClassName() + ".py", String.format("http://www.codeforces.com/contest/%s/problem/%s", contest.id, ps.getID()));
-			prefs.put(ps.getClassName() + "_test.py", String.format("http://www.codeforces.com/contest/%s/problem/%s", contest.id, ps.getID()));
-	        prefs.put("timeout." + ps.getClassName() + "_test.py", ps.getTimeLimit());
-	        prefs.flush();
-			IFile solutionFile = project.getFile(ps.getClassName()+ ".py");
-			solutionFile.create(new ByteArrayInputStream(PythonCodeGenerator.DEFAULT_CODE_TEMPLATE.getBytes()), false, null);
-			IFile testFile = project.getFile(ps.getClassName()+"_test.py");
+		IEclipsePreferences prefs = EclipseForcesPlugin.getProjectPrefs(project);
+		prefs.put(ProblemStatementView.CONTEST_KEY, contest.name);
+		List<IFile> testFiles = new ArrayList<IFile>();
+		for (ProblemStatement ps : getProblems()) {
+			prefs.put(ps.getClassName() + ".py",
+					String.format("http://www.codeforces.com/contest/%s/problem/%s", contest.id, ps.getID()));
+			prefs.put(ps.getClassName() + "_test.py",
+					String.format("http://www.codeforces.com/contest/%s/problem/%s", contest.id, ps.getID()));
+			prefs.put("timeout." + ps.getClassName() + "_test.py", ps.getTimeLimit());
+			prefs.flush();
+			IFile solutionFile = project.getFile(ps.getClassName() + ".py");
+			solutionFile.create(new ByteArrayInputStream(PythonCodeGenerator.DEFAULT_CODE_TEMPLATE.getBytes()), false,
+					null);
+			IFile testFile = project.getFile(ps.getClassName() + "_test.py");
 			testFile.create(new ByteArrayInputStream(getCodeGenerator().getTestsSource(ps).getBytes()), false, null);
 			testFiles.add(testFile);
 		}
@@ -64,7 +83,7 @@ public class PythonLanguageSupport extends LanguageSupport {
 
 	@Override
 	protected String getSolutionFileName(ProblemStatement ps) {
-		return ps.getClassName()+".py";
+		return ps.getClassName() + ".py";
 	}
 
 }
